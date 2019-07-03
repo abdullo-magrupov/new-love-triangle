f809ba6d585478228bd5e1e5465e0f5470b0b7f8
abdullo-magrupov <magrupov.abdullo@gmail.com>
2019-07-03 15:11:05 +0500

    identation fix

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-	let counter = 0;			
-		for (i = 0; i < preferences.length; i++) {
-			if (preferences[preferences[preferences[i] -1 ] - 1] === i + 1) {
-				counter++;
-			}
+	let counter = 0;	
+	
+	for (i = 0; i < preferences.length; i++) {
+		if (preferences[preferences[preferences[i] -1 ] - 1] === i + 1) {
+			counter++;
 		}
-			
+	}
+		
 	
 	return Math.floor(counter / 3);
 };
