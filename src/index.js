9b4c4ff2a47255ef873be3ef6371083e9f4126e7
abdullo-magrupov <magrupov.abdullo@gmail.com>
2019-07-03 15:51:13 +0500

    Comments were added and tabs were fixed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-	let counter = 0;	
-	
-	for (i = 0; i < preferences.length; i++) {
-		if (preferences[preferences[preferences[i] -1 ] - 1] === i + 1) {
-			counter++;
-		}
-	}
-		
-	
-	return Math.floor(counter / 3);
+  let counter = 0;
+
+  for (i = 0; i < preferences.length; i++) {
+    // Formula explanation
+    // preferences[i] value is current Spichonee, let's call preferences[i] equals A
+    // being in love triangle requires:
+    // Spichonee A to love Spichonee on (A - 1) index of preferences array, let's call preferences[A - 1] equals B
+    // In order to complete the love triangle,
+    // Spichonee B is required to love Spichonee on (B - 1) index of preferences array
+    // and preferences[B - 1] value must be equal to (i + 1) which is the position (not index) of Spichonee A
+    // In general (i + 1) gives us lover of current Spichonee,
+    // preferences[prefernces[i] - 1] gives us the Spichonee which is loved by current Spichonee
+
+    if (preferences[preferences[preferences[i] - 1] - 1] === i + 1) {
+      counter++;
+    }
+  }
+
+  // Counter was divided by 3 as each love triangle occures three times on the loop
+  return Math.floor(counter / 3);
 };
